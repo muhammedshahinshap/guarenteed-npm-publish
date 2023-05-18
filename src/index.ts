516f149cdb85156be7d0b32dcd5f59a7a98db940
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-05-18 17:59:29 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * from "./errors/req-validation";
-export * from "./errors/not-found";
-export * from "./errors/db-error";
-export * from "./errors/custErr";
+export * as ReqValidationerr from "./errors/req-validation";
+export * as NotFound from "./errors/not-found";
+export * as DbError from "./errors/db-error";
+export * as custErr from "./errors/custErr";
 
-export * from "./middleware/errorhandler";
-export * from "./middleware/isAuth";
-export * from "./middleware/requestValidationerr";
+export * as errorhandler from "./middleware/errorhandler";
+export * as isAuth from "./middleware/isAuth";
+export * as requestValidationerr from "./middleware/requestValidationerr";
