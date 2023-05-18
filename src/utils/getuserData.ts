a88ce7e244a0e5f2e250e4863e265ef15b2c016b
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-05-18 20:00:49 +0530

    updates

--- src/utils/getuserData.ts
+++ src/utils/getuserData.ts
@@ -1,8 +1,18 @@
 import { Types } from "mongoose";
-// import User from "../models/userMdl";
+const { MongoClient } = require("mongodb");
 export default async (id: Types.ObjectId) => {
   try {
-    return await User.findById({ _id: id }).select("-password");
+    const url = "mongodb://127.0.0.1:27017/";
+    const dbName = "auth";
+    const client = new MongoClient(url, { useUnifiedTopology: true });
+    client.connect(async (err: any) => {
+      if (err) {
+        console.error("Error connecting to MongoDB:", err);
+        return;
+      }
+      const db = client.db(dbName);
+      return await db.collection("users").findOne({ _id: id });
+    });
   } catch (error) {
     console.log(error);
     return false;
