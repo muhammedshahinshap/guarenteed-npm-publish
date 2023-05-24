8369f40add3fa901aad4af9c3c2dac10df58df5f
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-05-24 20:04:28 +0530

    updates

--- src/events/created-events/job-poster-data-event.ts
+++ src/events/created-events/job-poster-data-event.ts
@@ -2,5 +2,18 @@ import { ObjectId } from "mongodb";
 import { Subjects } from "../subjects/Subjects";
 export interface JobPosterEvents {
   subject: Subjects.jobPosterData;
-  data:any
+  data: {
+    _id: ObjectId;
+    fk_id: ObjectId;
+    type: string;
+    noOfYears: string;
+    tech: string;
+    salary: string;
+    discription: string;
+    image?: string;
+    status: boolean;
+    reportStatus: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
 }
