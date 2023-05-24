33f7538c6611dd2a33bc28b7b4377ccd2e8cf48a
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-05-24 10:56:18 +0530

    updates

--- src/events/base-events/base-listener.ts
+++ src/events/base-events/base-listener.ts
@@ -1,19 +1,20 @@
-import nats, { Message, Stan } from "node-nats-streaming";
+import { Message, Stan } from "node-nats-streaming";
 import { Subjects } from "../subjects/Subjects";
 interface Event {
   subject: Subjects;
   data: any;
 }
 export abstract class Listener<T extends Event> {
-  protected client: Stan;
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
   abstract onMessage(data: T["data"], msg: Message): void;
-  abstract subject: T["subject"];
-  protected ackWait: number = 5 * 1000;
+  protected client: Stan;
+  protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
     this.client = client;
   }
+
   subscriptionOptions() {
     return this.client
       .subscriptionOptions()
@@ -22,21 +23,24 @@ export abstract class Listener<T extends Event> {
       .setAckWait(this.ackWait)
       .setDurableName(this.queueGroupName);
   }
+
   listen() {
-    const subscribe = this.client.subscribe(
+    const subscription = this.client.subscribe(
       this.subject,
       this.queueGroupName,
       this.subscriptionOptions()
     );
-    subscribe.on("message", (msg: Message) => {
-      const parseData = this.parseMessage(msg);
-      this.onMessage(parseData, msg);
+
+    subscription.on("message", (msg: Message) => {
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
     });
   }
+
   parseMessage(msg: Message) {
     const data = msg.getData();
     return typeof data === "string"
       ? JSON.parse(data)
-      : JSON.parse(data.toString("utf-8"));
+      : JSON.parse(data.toString("utf8"));
   }
 }
